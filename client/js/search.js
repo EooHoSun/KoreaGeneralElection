a6efb2431af0fe7a3a70d6f166bc2bcd8a552644
EooHoSun <redysj0517@naver.com>
2020-02-11 15:48:03 +0900

    search commit

--- client/js/search.js
+++ client/js/search.js
@@ -1,11 +1,85 @@
-export default function Search(){
-    const search_ = function(geoJson){
+import 'leaflet/dist/leaflet.css'
+import * as L from 'leaflet'
+import 'leaflet-control-custom/Leaflet.Control.Custom'
+import autocomplete from 'autocompleter'
 
-    }
-
-    search.prototype = {
-
-    }
-
-    return search_;
+function setUl_(arr) {
+	if (arr.length === 0) return ''
+	let ulInnerHtml = ''
+	arr.forEach(element => {
+		ulInnerHtml += `<li class="s-li" onclick="">${element}</li>`
+	})
+	return ulInnerHtml
 }
+
+/**
+ * Search 정의
+ *
+ */
+function Search(data) {
+	this.data = data
+	this.search = {}
+	this.init()
+}
+
+Search.prototype.init = function() {
+	this.search = L.control.custom({
+		position: 'topleft',
+		content:
+			'<div class="input-group">' +
+			'    <input type="text" id="s-input" class="s-input" placeholder="Search">' +
+			'    <button class="s-button" type="button">Go!</button>' +
+			'    <div class="s-ul-div">' +
+			'        <ul id="s-ul" class="s-ul"></ul>' +
+			'    </div>' +
+			'</div>',
+		classes: '',
+		style: {
+			position: 'absolute',
+			left: '50px',
+			top: '0px',
+			width: '300px',
+		},
+		events: {
+			click(e) {
+			},
+			change(e) {
+			},
+		},
+	})
+}
+
+Search.prototype.setAutoComplete = function() {
+	const self = this
+	const input = document.getElementById('s-input')
+	const ul = document.getElementById('s-ul')
+	autocomplete({
+		input,
+		fetch: (text, update) => {
+			// eslint-disable-next-line no-param-reassign
+			text = text.toLowerCase()
+			// eslint-disable-next-line prettier/prettier
+			const suggestions = self.data.data.geoJson.features.filter(n =>
+				n.properties.elect_cd.toLowerCase().indexOf(text) > -1
+			)
+			if (suggestions.length === 0) return
+			// eslint-disable-next-line no-use-before-define
+			ul.innerHTML = setUl_(
+				suggestions.map(function(element) {
+					return element.properties.elect_cd
+				})
+			)
+			update(
+				suggestions.map(function(element) {
+					return element.properties.elect_cd
+				})
+			)
+		},
+		onSelect: item => {
+			input.value = item.label
+		},
+	})
+}
+
+
+export default Search
--- client/js/votemap.js
+++ client/js/votemap.js
@@ -3,7 +3,7 @@ import * as L from 'leaflet'
 import axios from 'axios'
 import 'leaflet-control-custom/Leaflet.Control.Custom'
 import markerIcon from '../img/marker.png'
-import search from 'leaflet-search';
+import Search from './search';
 
 /**
  * 정당 & 레이어 색상
@@ -26,7 +26,6 @@ function VoteMap(mapId) {
 	this.map = {}
 	this.mapId = mapId
 	this.markers = {}
-	//hosun 0210 추가
 	this.hjd = {}
 }
 
@@ -37,7 +36,7 @@ VoteMap.prototype.init = async function init() {
 	const zoom = 7 // init zoom
 	const center = L.latLng(36.1358642, 128.0785804) // init center
 
-	this._setHJD();
+	this._setHJD()
 	// Google Map
 	const googleMap = L.tileLayer('https://mt0.google.com/vt/lyrs=m&hl=kr&x={x}&y={y}&z={z}', {
 		attribution: `&copy; <a target="_blank" href="https://maps.google.com/maps?ll=${center.lat},${center.lng}&amp;z=13&amp;t=m&amp;hl=ko-KR&amp;gl=US&amp;mapclient=apiv3" title="Google 지도에서 이 지역을 보려면 클릭하세요." ><img alt="" src="https://maps.gstatic.com/mapfiles/api-3/images/google4.png" draggable="false"></a>`,
@@ -57,7 +56,7 @@ VoteMap.prototype.init = async function init() {
 	// 20대 선거구 그리기
 	await this._drawElect20Layer()
 
-	this._setSearch();
+	this._setSearch()
 
 	return this.map
 }
@@ -85,7 +84,7 @@ VoteMap.prototype._createGeolocButton = function() {
 					if (!self.markers.myloc) {
 						// Geolocation 객체를 사용
 						if (navigator.geolocation) {
-							const options = {	
+							const options = {
 								enableHighAccuracy: true, // 정확한 값 : true, 대략적인 값 : false
 								timeout: 10000, // 10 초이상 기다리지 않음.
 							}
@@ -149,66 +148,30 @@ VoteMap.prototype._drawElect20Layer = async function() {
 		.addTo(this.map)
 }
 
-
-
-//hosun 0210 추가
 /**
  * search box 만들기
  */
 VoteMap.prototype._setSearch = async function() {
-
-	
-	L.control.custom({
-		position: 'topleft',
-		content : '<div class="input-group">'+
-				  '    <input type="text" class="form-control input-sm" placeholder="Search">'+
-				  '    <span class="input-group-btn">'+
-				  '        <button class="btn btn-default btn-sm" type="button">Go!</button>'+
-				  '    </span>'+
-				  '</div>',
-		classes : '',
-		style   :
-		{
-			position: 'absolute',
-			left: '50px',
-			top: '0px',
-			width: '300px'
-		},
-		events: {
-			click : function(e){
-				console.log(e);
-			},
-			change : function(e){
-				console.log(e);
-			},
-			contextmenu : function(e){
-				console.log(e);
-			},
-			dbClick : function(e){
-				console.log(e);
-			}
-
-		}
-
-	})
-	.addTo(this.map);
-	
-
+	const searchBox = new Search(await axios.get('/api/data?type=20'))
+	searchBox.search.addTo(this.map)
+	searchBox.setAutoComplete()
 }
 
-
-VoteMap.prototype._setHJD = async function(){
-	const {data, status} = await axios.get("/api/data?type=20");
-	if(status != 200) return;
+/**
+ * 행정동 setting
+ */
+VoteMap.prototype._setHJD = async function() {
+	const { data, status } = await axios.get('/api/data?type=20')
+	if (status !== 200) return
 
 	this.hjd = L.geoJSON(data.geoJson, {
-		style : {
+		style: {
 			weight: 1,
-			color: "#00ff0000",
+			color: '#00ff0000',
 		},
-	});
-	this.hjd.addTo(this.map);
-	console.log(this.hjd);
+	})
+	this.hjd.addTo(this.map)
+	console.log(this.hjd)
 }
 
 export default VoteMap
